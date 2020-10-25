413c8363b977098c015d0b3bdc5abb32073e5329
Andres Magana <andresmaganajr@gmail.com>
2020-10-24 20:11:36 -0500

    added all the java files to the program

---
+++ src/FlightPlanner.java
@@ -0,0 +1,107 @@
+import acm.program.*;
+import acm.util.*;
+import java.io.*;
+import java.util.*;
+public class FlightPlanner extends ConsoleProgram {
+	
+	private HashMap<String,ArrayList<String>> flights;
+	private ArrayList<String> cities;
+	
+	/** Runs the program. */
+	public void run() {
+		println("Welcome to Flight Planner!");
+		
+		readFlightData("flights.txt");
+		
+		println("Here's a list of all the cities in our database:");
+		printCityList(cities);
+		
+		println("Let's plan a round-trip route!");
+		String startCity = readLine("Enter the starting city: ");
+		ArrayList<String> route = new ArrayList<String>();
+		route.add(startCity);
+		String currentCity = startCity;
+		String nextCity = "";
+		do {
+			nextCity = getNextCity(currentCity);
+			route.add(nextCity);
+			currentCity = nextCity;
+		}while (!nextCity.equals(startCity));
+		
+		printRoute(route);
+	}
+	
+	private void readFlightData(String filename) {
+		flights = new HashMap<String,ArrayList<String>>();
+		cities = new ArrayList<String>();
+		try (BufferedReader rd = new BufferedReader(new FileReader(filename))) {
+			String line = rd.readLine();
+			while (line != null) {
+				if (line.length() != 0) {
+					readFlightEntry(line);
+				}
+				line = rd.readLine();
+			}
+			
+		} catch (IOException ex) {
+			throw new ErrorException(ex);
+		}
+	}
+	
+	private void readFlightEntry(String line) {
+		int arrow = line.indexOf("->");
+		if (arrow == -1) {
+			throw new ErrorException("Illegal flight entry " + line);
+		}
+		String fromCity = line.substring(0, arrow).trim();
+		String toCity = line.substring(arrow + 2).trim();
+		defineCity(fromCity);
+		defineCity(toCity);
+		getDestinations(fromCity).add(toCity);
+	}
+	
+	private void defineCity(String cityName) {
+		if (!cities.contains(cityName)) {
+			cities.add(cityName);
+			flights.put(cityName, new ArrayList<String>());
+		}
+	}
+	
+	private ArrayList<String> getDestinations(String fromCity) {
+		return flights.get(fromCity);
+	}
+	
+	private String getNextCity(String city) {
+		ArrayList<String> destinations = getDestinations(city);
+		String nextCity = null;
+		while (true) {
+			println("From " + city + " you can fly directly to:");
+			printCityList(destinations);
+			String prompt = "Where do you want to go from " + city + "? ";
+			nextCity = readLine(prompt);
+			if (destinations.contains(nextCity)) {
+				return nextCity;
+			}
+			println("You can't get to that city by a direct flight.");
+		}
+	}
+	
+	private void printCityList(ArrayList<String> cityList) {
+		for(String city : cityList) {
+			println(" " + city);
+		}
+	}
+	private void printRoute(ArrayList<String> route) {
+		println("The route you've chosen is: ");
+		Boolean first = true;
+		for (String city: route) {
+			if (!first) {
+				print(" -> ");
+			}
+			print(city);
+			first = false;
+		}
+		println();
+	}
+	
+}
